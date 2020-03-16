948a2aa41b93dec6bb0ebabab52988baaf65e025
ftd.wild <ftd.wild@ftdwilddeiMac.local>
2020-03-17 00:09:01 +0800

    [add]添加整体编译框架，基于cmake

---
+++ src/task_queue_select.cpp
@@ -0,0 +1,5 @@
+//
+// Created by ftd.wild on 2020/3/16.
+//
+
+#include "task_queue_select.h"
---
+++ src/task_queue_select.h
@@ -0,0 +1,21 @@
+//
+// Created by ftd.wild on 2020/3/16.
+//
+
+#ifndef EVENTDRIVEN_TASK_QUEUE_SELECT_H
+#define EVENTDRIVEN_TASK_QUEUE_SELECT_H
+
+namespace ftdwild {
+
+class TaskQueue {
+public:
+    TaskQueue();
+    ~TaskQueue();
+};
+
+
+} //ftdwild
+
+
+
+#endif //EVENTDRIVEN_TASK_QUEUE_SELECT_H
